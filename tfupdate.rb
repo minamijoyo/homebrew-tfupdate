389f386abe6de00954eff69805a071127138e4dd
Masayuki Morita <minamijoyo@gmail.com>
2020-06-18 11:08:52 +0900

    Brew formula update for tfupdate version v0.4.0

--- tfupdate.rb
+++ tfupdate.rb
@@ -2,16 +2,16 @@
 class Tfupdate < Formula
   desc "Update version constraints in your Terraform configurations"
   homepage "https://github.com/minamijoyo/tfupdate"
-  version "0.3.6"
+  version "0.4.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/minamijoyo/tfupdate/releases/download/v0.3.6/tfupdate_0.3.6_darwin_amd64.tar.gz"
-    sha256 "40a871787538580849af10e927d2a713e8a7ca5ade3ea837bf58c0d7476b6e2b"
+    url "https://github.com/minamijoyo/tfupdate/releases/download/v0.4.0/tfupdate_0.4.0_darwin_amd64.tar.gz"
+    sha256 "3294e5e245d7c045803c6ad97c33094d07ac8cb442e9dd9400ba5320eb702155"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/minamijoyo/tfupdate/releases/download/v0.3.6/tfupdate_0.3.6_linux_amd64.tar.gz"
-      sha256 "707a611dbb07ac2fd8a80a929a6d1a5b5bf89870ff2d0ece9763f1dfa30ece54"
+      url "https://github.com/minamijoyo/tfupdate/releases/download/v0.4.0/tfupdate_0.4.0_linux_amd64.tar.gz"
+      sha256 "7bbff9aed625aaf2a19e8268b10bdf9d40892ede19078d90d97e19798671f2f7"
     end
   end
 
